1d3e6f06410097469500a2cf7b8aff03bb39370b
DC-21 <cholahkuboko@gmail.com>
2023-06-12 11:41:56 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,52 @@
-import React, { useState } from 'react'
-import { TfiMenu } from 'react-icons/tfi'
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-    const [ open, setOpen ] = useState(false);
-    const handleNav = () => {
-        setOpen((prevOpen) => !prevOpen);
-    }
+  const handleNav = () => {
+    setOpen((prevOpen) => !prevOpen);
+  }
+
   return (
-    <div className="w-full flex justify-between md:px-8 px-6 text-white md:py-6 py-4" style={{backgroundColor: '#133973'}}>
+    <div className="w-full flex justify-between md:px-8 px-6 text-white md:py-6 py-4" style={{ backgroundColor: '#133973' }}>
       <div>
         <a href=''>DiCE</a>
       </div>
-      <div className="md:block hidden">
-        <ul className="flex justify-center gap-8">
-            <li>
-                <a href=''>Home</a>
-            </li>
-            <li>
-                <a href=''>Services</a>
-            </li>
-            <li>
-                <a href=''>Portfolio</a>
-            </li>
+      <div className="">
+        <ul className="justify-center gap-8 md:hidden block">
+          <li>
+            <a href=''>Home</a>
+          </li>
+          <li>
+            <a href=''>Services</a>
+          </li>
+          <li>
+            <a href=''>Portfolio</a>
+          </li>
         </ul>
       </div>
       <div className="flex gap-2">
         <button className="md:block hidden">Hire Me</button>
-        <TfiMenu className="md:hidden block"/>
-        { open ? ( <div className="md:block hidden">
-        <ul className="flex justify-center gap-8">
-            <li>
+        <button onClick={handleNav} className="md:hidden block"><FaBars /></button>
+        {open ? (
+          <div className="md:block hidden">
+            <ul className="flex justify-center gap-8">
+              <li>
                 <a href=''>Home</a>
-            </li>
-            <li>
+              </li>
+              <li>
                 <a href=''>Services</a>
-            </li>
-            <li>
+              </li>
+              <li>
                 <a href=''>Portfolio</a>
-            </li>
-        </ul>
-      </div> ): null }
+              </li>
+            </ul>
+          </div>
+        ) : null}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
