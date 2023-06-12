cae6200fac01e50b78c6ea9253b9b355d9099f23
DC-21 <cholahkuboko@gmail.com>
2023-06-12 15:14:35 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation'
-import girl from '../images/girl.png'
+import own from '../images/own.png'
 
 
 const Hero = () => {
   return (
-    <section className="h-screen w-full sticky" style={{ background: 'linear-gradient(to right, #5b80d7, #2d177d)' }}  id="home">
+    <section className="min-h-screen w-full sticky" style={{ background: 'linear-gradient(to right, #5b80d7, #2d177d)' }}  id="home">
       <div className="px-6 flex md:flex-row flex-col w-full md:justify-between justify-center">
         <div className="justify-center items-center md:w-1/2 ">
-          <div>
+          <div className="pt-40">
           <p className="pt-10 text-lg font-bold text-center text-indigo-100">I'm Chola Kuboko</p>
-            <div className="text-center">
+            <div className="text-center pt-4">
              <TypeAnimation
                sequence={[
                 'Web Developer', 2000,
@@ -22,11 +22,11 @@ const Hero = () => {
                 wrapper="span"
                />
               </div>
-              <p className="mb-4 w-full text-center md:text-left text-white pt-4 text:sm md:text-md font-medium-bold text-large font-medium py-0 px-0">Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
+              <p className="mb-4 w-full text-center items-center md:text-left text-white pt-4 text:sm md:text-md font-medium-bold text-large font-medium py-0 px-0">Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
           </div>
         </div>
         <div className="w-full md:w-1/2 h-full">
-        <img className="w-full h-85  py-12 object-cover object-center rounded-md"  src={girl}/>
+        <img className="w-full h-[10%]  py-12 object-cover object-center rounded-md"  src={own}/>
         </div>
       </div>
     </section>
