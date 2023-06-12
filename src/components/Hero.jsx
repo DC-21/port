18a0ca135aeb1c8bdf1b8b1937861406c562ee16
DC-21 <cholahkuboko@gmail.com>
2023-06-12 13:52:49 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
@@ -6,6 +7,15 @@ const Hero = () => {
       <div className="px-6 flex w-full justify-between">
         <div className="justify-center items-center w-1/2">
             <p className="pt-10 items-center">I'm Chola Kuboko</p>
+            <TypeAnimation sequence={[
+                'Web Developer', 2000,
+                'AI Chatbot Developer', 2000,
+                'Web Penetration Tester', 2000
+            ]}
+            className="text-blue-900 font-bold text-4xl mb-8"
+            repeat={Infinity}
+            wrapper="span"
+            />
         </div>
         <div className="justify-center items-center w-1/2">
         <p className="pt-10 items-center">I'm Chola Kuboko</p>
