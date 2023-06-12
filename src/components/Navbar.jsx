9ac62029ec224752d2d7bac66c6d9b12f372da0f
DC-21 <cholahkuboko@gmail.com>
2023-06-12 11:29:13 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TfiMenu } from 'react-icons/tfi'
 
 const Navbar = () => {
+
+    const [ open, setOpen ] = useState(false);
+    const handleNav = () => {
+        setOpen((prevOpen) => !prevOpen);
+    }
   return (
-    <div className="w-full flex justify-between px-8 text-white py-6" style={{backgroundColor: '#133973'}}>
+    <div className="w-full flex justify-between md:px-8 px-6 text-white md:py-6 py-4" style={{backgroundColor: '#133973'}}>
       <div>
         <a href=''>DiCE</a>
       </div>
-      <div>
+      <div className="md:block hidden">
         <ul className="flex justify-center gap-8">
             <li>
                 <a href=''>Home</a>
@@ -19,8 +25,22 @@ const Navbar = () => {
             </li>
         </ul>
       </div>
-      <div>
-        <button>Hire Me</button>
+      <div className="flex gap-2">
+        <button className="md:block hidden">Hire Me</button>
+        <TfiMenu className="md:hidden block"/>
+        { open ? ( <div className="md:block hidden">
+        <ul className="flex justify-center gap-8">
+            <li>
+                <a href=''>Home</a>
+            </li>
+            <li>
+                <a href=''>Services</a>
+            </li>
+            <li>
+                <a href=''>Portfolio</a>
+            </li>
+        </ul>
+      </div> ): null }
       </div>
     </div>
   )
