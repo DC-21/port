f13beb511099f8d654bd7c04d85acf6f19f5255c
DC-21 <cholahkuboko@gmail.com>
2023-06-12 13:25:31 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Portfolio from './components/Portfolio'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Hero/>
+    <Services/>
+    <Portfolio/>
     </>
   )
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Services
