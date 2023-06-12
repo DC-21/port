432d97770c199f93b29eeaa80b61732a1a279cb0
DC-21 <cholahkuboko@gmail.com>
2023-06-12 14:26:32 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,19 +4,24 @@ import { TypeAnimation } from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <section className="h-screen w-full" style={{ backgroundColor: '#5b80d7'}} id="home">
+    <section className="h-screen w-full" style={{ background: 'linear-gradient(to right, #5b80d7, #2d177d)' }}  id="home">
       <div className="px-6 flex w-full justify-between">
         <div className="justify-center items-center w-1/2">
             <p className="pt-10 items-center">I'm Chola Kuboko</p>
-            <div><TypeAnimation sequence={[
-                'Web Developer', 2000,
-                'AI Chatbot Developer', 2000,
-                'Web Penetration Tester', 2000
-            ]}
-            className="text-blue-900 font-bold text-4xl mb-8"
-            repeat={Infinity}
-            wrapper="span"
-            /></div>
+            <div>
+  <TypeAnimation
+    sequence={[
+      'Web Developer', 2000,
+      'AI Chatbot Developer', 2000,
+      'Web Penetration Tester', 2000
+    ]}
+    style={{ fontSize: '14px' }}
+    className="text-white"
+    repeat={Infinity}
+    wrapper="span"
+  />
+</div>
+
         </div>
         <div className="justify-center items-center w-1/2">
         <p className="pt-10 items-center">I'm Chola Kuboko</p>
