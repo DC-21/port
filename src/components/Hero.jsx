2dfbd4c8013d1b50cd61a9631cfbdeb6f9cb8951
DC-21 <cholahkuboko@gmail.com>
2023-06-12 13:45:36 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className="h-screen w-full" style={{ backgroundColor: '#5b80d7'}} id="home">
-      <div className="px-6">
+      <div className="px-6 flex w-full justify-between">
         <div className="justify-center items-center w-1/2">
             <p className="pt-10 items-center">I'm Chola Kuboko</p>
         </div>
-        <div className="justify-center items-center w-1/2"></div>
+        <div className="justify-center items-center w-1/2">
+        <p className="pt-10 items-center">I'm Chola Kuboko</p>
+        </div>
       </div>
     </section>
   )
