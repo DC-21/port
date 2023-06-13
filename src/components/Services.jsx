45f1de07b10fdd53a953ae1f02e0bc8047958eb8
DC-21 <cholahkuboko@gmail.com>
2023-06-13 14:12:33 +0200

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -35,8 +35,19 @@ const Services = () => {
           />
          </div>
          </div>
-         <div className="h-1/3">
-          <p>Junior Web Developer</p>
+         <div className="w-full flex">
+          <div className="w-1/2 h-1/3">
+         <p>Junior Web Developer</p>
+         </div>
+         <div className="w-1/2">
+         <Player
+            autoplay
+            loop
+            src="https://assets6.lottiefiles.com/packages/lf20_q8ND1A8ibK.json"
+            style={{ maxWidth: '600px', width: '100%', height: 'auto' }}
+            className="z-10 hover:-translate-y-4 transition-all ease-in-out duration-300 cursor-pointer"
+          />
+         </div>
          </div>
         </div>
         </div>
