06391b55dc164e969bc4dd76a27808d696238218
DC-21 <cholahkuboko@gmail.com>
2023-06-14 00:49:41 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="px-6 flex md:flex-row flex-col w-full md:justify-between justify-center">
         <div className="justify-center items-center">
           <div className="md:pt-20 pt-1">
-          <p className="md:pt-10 pt-4 text-3xl font-bold text-center underline" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>I'm Chola Kuboko</p>
+          <p className="md:pt-10 pt-4 text-3xl pl-10 font-bold text-center underline" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>I'm Chola Kuboko</p>
             <div className="text-center md:pt-4 pt-2">
              <TypeAnimation
                sequence={[
@@ -17,13 +17,13 @@ const Hero = () => {
                 'An AI Chatbot Developer', 2000,
                 'A Web Penetration Tester', 2000
                    ]}
-                className="text-2xl font-bold" 
+                className="text-2xl pl-10 font-bold" 
                 repeat={Infinity}
                 wrapper="span"
                 style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}
                />
               </div>
-              <p className="mb-4 px-8 ml-4 w-full text-center items-center md:text-left pt-4 text:sm md:text-md text-large font-lg py-0" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
+              <p className="mb-4 pl-20 px-8 ml-4 w-full text-center items-center md:text-left pt-4 text:sm md:text-md text-large font-lg py-0" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
             </div>
             <div className="text-center md:block items-center">
               <button className="hover:bg-blue-300 text-white text-lg font-bold py-2 px-3 rounded" style={{ backgroundColor: '#2d177d' }}>
