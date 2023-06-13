d301c78a21524b9a79a0642cf98f6f19416fabae
DC-21 <cholahkuboko@gmail.com>
2023-06-13 23:52:18 +0200

    first commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <section className="h-screen w-full" style={{ background: 'linear-gradient(to right, #5b80d7, #2d177d)' }} id="portfolio">
+    <section className="h-screen w-full" style={{ background: 'linear-gradient(to right, #fafafa, #2d177d)' }} id="portfolio">
       <div>
         <div className="w-full">
           <div></div>
