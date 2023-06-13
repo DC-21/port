0c2c455f1197753a736f883c825af99f639a0f52
DC-21 <cholahkuboko@gmail.com>
2023-06-13 23:38:32 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="px-6 flex md:flex-row flex-col w-full md:justify-between justify-center">
         <div className="justify-center items-center md:w-1/2 ">
           <div className="md:pt-40 pt-1">
-          <p className="md:pt-10 pt-4 text-2xl font-bold text-center" style={{ color: '#1c1531'}}>I'm Chola Kuboko</p>
+          <p className="md:pt-10 pt-4 text-3xl font-bold text-center underline" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>I'm Chola Kuboko</p>
             <div className="text-center md:pt-4 pt-2">
              <TypeAnimation
                sequence={[
@@ -17,15 +17,15 @@ const Hero = () => {
                 'An AI Chatbot Developer', 2000,
                 'A Web Penetration Tester', 2000
                    ]}
-                className="text-2xl font-bold underline" 
+                className="text-2xl font-bold" 
                 repeat={Infinity}
                 wrapper="span"
                 style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}
                />
               </div>
-              <p className="mb-4 ml-4 w-full text-center items-center md:text-left pt-4 text:sm md:text-md font-medium-bold text-large font-medium py-0 px-0" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
+              <p className="mb-4 ml-4 w-full text-center items-center md:text-left pt-4 text:sm md:text-md text-large font-lg py-0 px-0" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
           </div>
-          <div className="text-center md:block hidden items-center"><button className="text-white text-l font-bold bg-blue-400 py-2 px-3 rounded hover:bg-blue-300">Email Me</button></div>
+          <div className="text-center md:block hidden items-center"><button className="text-white text-l font-bold py-2 px-3 rounded hover:bg-blue-300" style={{ backgroundColor: '#2d177d' }}>Email Me</button></div>
         </div>
         <div className="w-full justify-center md:w-1/2 h-full md:pt-0 pt-4">
         
