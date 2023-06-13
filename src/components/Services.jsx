6c9b0c07f05a524ecdb4ba7fd91692f423f9d9d4
DC-21 <cholahkuboko@gmail.com>
2023-06-13 14:08:26 +0200

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -21,19 +21,20 @@ const Services = () => {
           />
          </div>
           </div>
-          <div>
-          <div className="h-1/3">
+          <div className="w-full flex">
+          <div className="w-1/2 h-1/3">
          <p>Junior Web Developer</p>
          </div>
          <div className="w-1/2">
          <Player
             autoplay
             loop
-            src="https://assets1.lottiefiles.com/packages/lf20_w51pcehl.json"
+            src="https://assets10.lottiefiles.com/packages/lf20_ndt8zfny.json"
             style={{ maxWidth: '600px', width: '100%', height: 'auto' }}
             className="z-10 hover:-translate-y-4 transition-all ease-in-out duration-300 cursor-pointer"
           />
-         </div></div>
+         </div>
+         </div>
          <div className="h-1/3">
           <p>Junior Web Developer</p>
          </div>
