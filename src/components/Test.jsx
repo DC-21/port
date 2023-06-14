3f4f270a12af91b67d3778b1e3c0349b8b1a7c25
DC-21 <cholahkuboko@gmail.com>
2023-06-14 16:55:06 +0200

    first commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import WebsiteSlider from './WebsiteSlider';
+
+const App = () => {
+  return (
+    <div>
+      {/* Other components */}
+      <WebsiteSlider />
+      {/* Other components */}
+    </div>
+  );
+};
+
+export default App;
