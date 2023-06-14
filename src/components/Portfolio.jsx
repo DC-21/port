9d5237845f22c6c305ec11cce4580c9a59c59142
DC-21 <cholahkuboko@gmail.com>
2023-06-14 16:38:24 +0200

    first commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <section class="text-gray-600 body-font">
+    <section class="text-gray-600 body-font" style={{ background: 'linear-gradient(to right, #fafafa, #2d177d)' }}>
   <div class="container px-5 py-24 mx-auto">
     <div class="text-center mb-20">
       <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Raw Denim Heirloom Man Braid</h1>
