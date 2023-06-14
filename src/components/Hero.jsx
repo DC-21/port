b92d00bd8a1fef83fdfb6662971b8ce052959d82
DC-21 <cholahkuboko@gmail.com>
2023-06-14 08:34:22 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation'
-import me from '../images/me.jpeg'
+import me from '../images/own.png'
 
 
 const Hero = () => {
+
+  const containerStyle = {
+    backgroundImage: `url(${me})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  };
+
   return (
-    <section className="h-screen w-full" style={{ background: 'linear-gradient(to right, #fafafa, #2d177d)' }}  id="home">
+    <section className="h-screen w-full" style={containerStyle}  id="home">
       <div className="px-6 flex md:flex-row flex-col w-full md:justify-between justify-center">
         <div className="justify-center items-center">
           <div className="md:pt-20 pt-1">
-          <p className="md:pt-10 pt-4 text-3xl pl-10 font-bold text-center underline" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>I'm Chola Kuboko</p>
+          <p className="md:pt-10 pt-4 text-3xl md:pl-10 pl-0 font-bold text-center underline" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>I'm Chola Kuboko</p>
             <div className="text-center md:pt-4 pt-2">
              <TypeAnimation
                sequence={[
@@ -23,7 +35,7 @@ const Hero = () => {
                 style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}
                />
               </div>
-              <p className="mb-4 pl-20 px-8 ml-4 w-full text-center items-center md:text-left pt-4 text:sm md:text-md text-large font-lg py-0" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
+              <p className="mb-4 md:pl-20 pl-0 px-8 ml-4 w-full text-center items-center md:text-left pt-4 text:sm md:text-md text-large font-lg py-0" style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}>Am a freelancer web developer with a passion for cybersecurity and AI chatbot Development.</p>
             </div>
             <div className="text-center md:block items-center">
               <button className="hover:bg-blue-300 text-white text-lg font-bold py-2 px-3 rounded" style={{ backgroundColor: '#2d177d' }}>
