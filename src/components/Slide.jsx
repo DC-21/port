7cdafba1c3be835d70fffa441f468a5f4a1494e0
DC-21 <cholahkuboko@gmail.com>
2023-06-14 16:54:30 +0200

    first commit

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import Slider from 'react-slick';
 
-
 const WebsiteSlider = () => {
-    // Define the websites to be embedded
-    const websites = [
-      'https://ai-chola.netlify.app/'
-    ];
-  
-    // Configure the settings for the slider
-    const sliderSettings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-  
-    return (
-        <div className="slider-container">
-          <Slider {...sliderSettings}>
-            {websites.map((website, index) => (
-              <iframe
-                key={index}
-                title={`Website ${index + 1}`}
-                src={website}
-                className="website-iframe"
-              />
-            ))}
-          </Slider>
-        </div>
-      );
-    };
+  // Define the websites to be embedded
+  const websites = [
+    'https://ai-chola.netlify.app/'
+  ];
+
+  // Configure the settings for the slider
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="slider-container">
+      <Slider {...sliderSettings}>
+        {websites.map((website, index) => (
+          <iframe
+            key={index}
+            title={`Website ${index + 1}`}
+            src={website}
+            className="website-iframe"
+          />
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default WebsiteSlider;
