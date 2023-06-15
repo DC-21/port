17774d6e3ef4829e59852c31f10fae356db6c86c
DC-21 <cholahkuboko@gmail.com>
2023-06-15 11:31:11 +0200

    first commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
     <section className="text-gray-600 body-font min-h-screen w-full rounded">
       <div className="container py-8 mx-auto flex flex-col w-full h-screen">
         <div className="px-4 py-4 flex justify-center rounded" style={{ background: 'linear-gradient(to right, #c562b3, #a1d0f2)' }}>
-          <h1 className="text-indigo-700 text-2xl font-bold">My Projects</h1>
+          <h1 className="text-indigo-700 text-2xl font-bold">My Portfolio</h1>
         </div>
         <div className="container px-5 py-8 mx-auto flex flex-wrap rounded" >
           <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded overflow-hidden min-h-screen md:pr-4">
@@ -15,6 +15,9 @@ const Portfolio = () => {
             </div>
           </div>
           <div className="flex flex-col flex-wrap rounded-md h-screen lg:py-6 md:py-10 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center md:pl-4" style={{ background: 'linear-gradient(to right, #fff, #a1d0f2)' }}>
+            <div className="flex flex-col  justify-center rounded mb-10 lg:items-start items-center">
+             <h1 className="text-indigo-700 text-2xl font-bold pt-6">My Projects</h1>
+            </div>
             <div className="flex flex-col mb-10 lg:items-start items-center md:pt-10 pt-0">
               <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
@@ -43,13 +46,8 @@ const Portfolio = () => {
                 </svg>
               </div>
               <div className="flex-grow">
-                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7" />
-                  </svg>
-                </a>
+                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">MyAi</h2>
+                <p className="leading-relaxed text-base">MyAi is next-generation Reminder based Ai that is going to run on mobile phones. Its under development now.</p>
               </div>
             </div>
           </div>
