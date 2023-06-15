e3a90927c8b51c13533f3735ccca18e5ea9ac4bd
DC-21 <cholahkuboko@gmail.com>
2023-06-15 10:52:50 +0200

    first commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,12 +9,12 @@ const Portfolio = () => {
           <h1 className="text-indigo-700 text-2xl font-bold">My Projects</h1>
         </div>
         <div className="container px-5 py-8 mx-auto flex flex-wrap rounded" >
-          <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded overflow-hidden">
-            <div className="h-screen bg-indigo-400 rounded-md">
+          <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded overflow-hidden min-h-screen">
+            <div className="bg-indigo-400 rounded-md">
               <Slide className="w-full h-full" />
             </div>
           </div>
-          <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center" style={{ background: 'linear-gradient(to right, #c562b3, #a1d0f2)' }}>
+          <div className="flex flex-col flex-wrap h-screen lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center" style={{ background: 'linear-gradient(to right, #c562b3, #a1d0f2)' }}>
             <div className="flex flex-col mb-10 lg:items-start items-center">
               <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
@@ -23,24 +23,6 @@ const Portfolio = () => {
               </div>
               <div className="flex-grow">
                 <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7" />
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div className="flex flex-col mb-10 lg:items-start items-center">
-              <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
-                  <circle cx="6" cy="6" r="3" />
-                  <circle cx="6" cy="18" r="3" />
-                  <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
-                </svg>
-              </div>
-              <div className="flex-grow">
-                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
                 <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
                 <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                   <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
